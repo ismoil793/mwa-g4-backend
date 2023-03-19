757dd3e402f9d69cb3979b0bd62f651bad1a70b6
birukyemane <bir_yem@yahoo.com>
2023-03-18 22:21:09 -0500

    - add getMyAutomobiles controller

--- controllers/automobile.controller.js
+++ controllers/automobile.controller.js
@@ -1,50 +1,60 @@
-const Automobile = require('../models/automible.model');
+const Automobile = require("../models/automible.model");
 
 async function getAllAutomobiles(req, res, next) {
-    try {
-        const automobiles = await Automobile.find({})
-        res.json({data: automobiles})
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const automobiles = await Automobile.find({});
+    res.json({ data: automobiles });
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function addAutomobile(req, res, next) {
-    try {
-        const payload = req.body
-        const result = await Automobile.create(payload)
-        res.json({data: result})
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const payload = req.body;
+    const result = await Automobile.create(payload);
+    res.json({ data: result });
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function getAutoById(req, res, next) {
-    try {
-        const { auto_id: autoId } = req.params
-        const result = await Automobile.findOne({_id: autoId})
-        res.json({data: result})
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const { auto_id: autoId } = req.params;
+    const result = await Automobile.findOne({ _id: autoId });
+    res.json({ data: result });
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function updateAutoById(req, res, next) {
-    try {
-        const { auto_id: autoId } = req.params
-        const result = await Automobile.updateOne(
-            {_id: autoId},
-            {_id: autoId, ...req.body}
-        )
-        res.json({data: result})
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const { auto_id: autoId } = req.params;
+    const result = await Automobile.updateOne(
+      { _id: autoId },
+      { _id: autoId, ...req.body }
+    );
+    res.json({ data: result });
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function getMyAutomobiles(req, res, next) {
+  try {
+    const automobiles = await Automobile.find({});
+    res.json({ data: automobiles });
+  } catch (e) {
+    next(e);
+  }
 }
 
 module.exports = {
-    getAllAutomobiles,
-    addAutomobile,
-    getAutoById,
-    updateAutoById
-}
+  getAllAutomobiles,
+  addAutomobile,
+  getAutoById,
+  updateAutoById,
+  getMyAutomobiles
+};
