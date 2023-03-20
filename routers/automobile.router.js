d4c7a2ba4b4d69a0322e86fbe7cb7e48320e1d8c
Tushar Ghosh <tusharghosh09006@gmail.com>
2023-03-19 22:23:18 -0500

    created purchase api

--- routers/automobile.router.js
+++ routers/automobile.router.js
@@ -7,37 +7,40 @@ const {
   getAutoById,
   getMyAutomobiles,
   updateAutoById,
-    deleteAutoById,
-    uploadImage,
+  deleteAutoById,
+  uploadImage,
+  autoPurchasedList,
 } = require("../controllers/automobile.controller");
 const offersRouter = require("./offers.router");
-const router = express.Router();
+
+const router = express.Router({ mergeParams: true });
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '..', 'public', 'uploads'))
-    },
-    fileFilter: (req, file, cb) => {
-        if (file.mimeType !== 'image/jpeg') {
-            return cb(new Error('Wrong file type'))
-        }
-        cb(null, true)
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`
-        cb(null, `${file.fieldname}-${uniqueSuffix}.jpg`)
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "..", "public", "uploads"));
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimeType !== "image/jpeg") {
+      return cb(new Error("Wrong file type"));
     }
-})
+    cb(null, true);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${file.fieldname}-${uniqueSuffix}.jpg`);
+  },
+});
 
-const upload = multer({storage, limits: {fileSize: 5 * 1024 * 1024}})
+const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 
-router.get('/', getAllAutomobiles) // GET /automobiles
-router.post('/', addAutomobile)
+router.get("/", getAllAutomobiles); // GET /automobiles
+router.post("/", addAutomobile);
 router.get("/myAutomobiles", getMyAutomobiles);
-router.get('/:auto_id', getAutoById)
-router.put('/:auto_id', updateAutoById)
-router.delete('/:auto_id', deleteAutoById)
-router.post('/:auto_id/upload', upload.array('picture', 4), uploadImage)
+router.get("/:auto_id", getAutoById);
+router.put("/:auto_id", updateAutoById);
+router.delete("/:auto_id", deleteAutoById);
+router.post("/:auto_id/upload", upload.array("picture", 4), uploadImage);
+router.get("/purchased", autoPurchasedList);
 
 router.use("/:auto_id/offers/", offersRouter);
 module.exports = router;
