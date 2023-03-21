f52c5e66c04a5a3f4a2b1177920f4079302e73aa
Tushar Ghosh <tusharghosh09006@gmail.com>
2023-03-21 14:41:45 -0500

    unique the email

--- models/users.model.js
+++ models/users.model.js
@@ -1,24 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  email: String,
+  email: {
+    type: String,
+    required: true,
+    match: /.+\@.+\..+/,
+    unique: true,
+  },
   password: String,
   fullname: String,
   location: {
     type: {
       type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: false
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: false,
     },
     coordinates: {
       type: [Number],
       required: false,
       sparse: true,
-    }
-  }
+    },
+  },
 });
 
-userSchema.index({location: '2dsphere'});
+userSchema.index({ location: "2dsphere" });
 
 module.exports = mongoose.model("Users", userSchema);
-
